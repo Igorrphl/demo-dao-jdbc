383f047fc0f1e43229c2691a84d5d4c4b37a0a0c
Igor Rodrigues <igrrph@gmail.com>
2024-01-17 00:16:50 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	//AQUI ESTÃO TODOS AS OPERAÇÕES ESTÁTICAS PARA OPERAR OS "DAOS"
+	
+	//Método retorna um tipo da interface, mas internamente ela vai retornar uma implementação
+	public static SellerDao createSellerDao() {
+				return new SellerDaoJDBC();
+	}
+
+}
